c93eb0bea0f0be3a485bf518372a91b7abe3ef23
rawleyfowler <rawleyfowler@proton.me>
2024-09-06 12:25:21 -0500

    [Example] Add DBI to Dancer2 example

--- examples/dancer2/dancer2.pl
+++ examples/dancer2/dancer2.pl
@@ -1,18 +1,24 @@
 use strict;
 use warnings;
 use Dancer2;
+use DBI;
 
 BEGIN {
 # would usually be in config.yml
-  set plugins => {SlapbirdAPM => {key => '01J65HDRHG6P0Z9CJ22FG7FMCGd2-app'}};
+  set plugins => {SlapbirdAPM => {key => '01J73YVM6MZX5AG93FCEZWD57Emy-app'}};
 }
 
 use Dancer2::Plugin::SlapbirdAPM;
 
+my $dbh = DBI->connect('dbi:SQLite:dbname=file.db','','');
+
 set port => 3030;
 
 get '/' => sub {
-  'Hello World';
+  my $sth = $dbh->prepare(q[SELECT time('now');]);
+  $sth->execute();
+  my $time = $sth->fetch->[0];
+  'Hello World! It is ' . $time . " o'clock";
 };
 
 get '/foo' => sub {
