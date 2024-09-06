bd7e06110d992c95abfac749e3ef025625f78c88
rawleyfowler <rawleyfowler@proton.me>
2024-09-06 13:27:00 -0500

    [Examples] Add more DBI stuff to dancer2

--- examples/dancer2/dancer2.pl
+++ examples/dancer2/dancer2.pl
@@ -5,24 +5,33 @@ use DBI;
 
 BEGIN {
 # would usually be in config.yml
-  set plugins => {SlapbirdAPM => {key => '01J73YVM6MZX5AG93FCEZWD57Emy-app'}};
+  set plugins => {SlapbirdAPM => {key => '<YOUR API KEY>'}};
 }
 
 use Dancer2::Plugin::SlapbirdAPM;
 
-my $dbh = DBI->connect('dbi:SQLite:dbname=file.db','','');
+my $dbh = DBI->connect('dbi:SQLite:dbname=file.db', '', '');
+
+$dbh->do('create table if not exists names (name VARCHAR, age INT)');
 
 set port => 3030;
 
 get '/' => sub {
-  my $sth = $dbh->prepare(q[SELECT time('now');]);
+  my $sth = $dbh->prepare(q[select time('now');]);
   $sth->execute();
   my $time = $sth->fetch->[0];
+
   'Hello World! It is ' . $time . " o'clock";
 };
 
-get '/foo' => sub {
-  'flarb';
+get '/name/:name' => sub {
+  my $name = route_parameters->get('name');
+  my $sth  = $dbh->prepare('insert into names (name, age) values (?, ?)');
+  $sth->execute($name, 23);
+  $sth = $dbh->prepare(q[select name from names where name like ?]);
+  $sth->execute($name);
+  my $name_from_db = $sth->fetch->[0];
+  return 'Inserted your name! ' . $name_from_db;
 };
 
 dance;
