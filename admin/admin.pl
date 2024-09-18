5fbf07c09e781d8aa6ebf03a802a50a47edc033e
rawleyfowler <rawleyfowler@proton.me>
2024-09-17 21:51:44 -0500

    [admin] Slap something together

--- admin/admin.pl
+++ admin/admin.pl
@@ -3,11 +3,14 @@ use warnings;
 
 use Plack::Builder;
 use Plack::Response;
+use HTML::Tiny;
 use DBI;
 use Dotenv -load;
 
 my $dbh = DBI->connect($ENV{ADMIN_DB_DSN}, $ENV{ADMIN_DB_USERNAME},
   $ENV{ADMIN_DB_PASSWORD}, {});
+
+my $html = HTML::Tiny->new;
 
 my $app = sub {
   my ($env) = @_;
@@ -18,12 +21,31 @@ my $app = sub {
   }
   my $response = Plack::Response->new(200);
   $response->content_type('text/html');
-  my $users = $dbh->selectall_arrayref('select * from users', {Slice => {}});
-  $response->body(map { $_->{name} } @$users);
+  my $summaries = $dbh->selectall_arrayref(
+    'select name, count(ht.http_transaction_id) as total from applications a left join http_transactions ht on a.application_id = ht.application_id group by name',
+    {Slice => {}}
+  );
+
+  $response->body($html->html([
+    $html->head($html->title('SlapbirdAPM statistics page')),
+    $html->body([
+      $html->h1('Slapbird insights'),
+      $html->div([
+        map { $html->p([$_->{name}, ' - ', $_->{total}]) } @$summaries
+      ])
+    ])
+  ]));
+
   return $response->finalize();
+};
+
+my $authen = sub {
+  my ($username, $password) = @_;
+  return $username eq $ENV{ADMIN_USERNAME} && $password eq $ENV{ADMIN_PASSWORD};
 };
 
 builder {
   enable 'SlapbirdAPM';
+  enable 'Auth::Basic', authenticator => $authen;
   $app;
 };
